e86970504bae7b18260406879fbf89b259dcde76
Muhammed <muhammeddurmaz.ce@gmail.com>
2026-09-02 08:45:59 +0300

    main.py rename, 2nd lesson, usage of parser and chain

---
+++ atil, bölüm 4/simplemessageWithOutputParser.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+import os
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.6-flash",
+    api_key = os.getenv("GEMINI_API_KEY"),
+    base_url = os.getenv("BASE_URL"),
+    temperature=0.7,
+)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="What is the capital of France?")
+]
+
+parser = StrOutputParser()
+
+chain = llm | parser
+
+if __name__ == "__main__":
+    print(chain.invoke(messages))
